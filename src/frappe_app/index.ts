16f849b690df9c045555914a19fd188fdbe3fb5d
Sumit Jain <jainmit23@gmail.com>
2023-02-23 12:16:08 +0530

    docs:token added in frappeapp

--- src/frappe_app/index.ts
+++ src/frappe_app/index.ts
@@ -10,25 +10,37 @@ export class FrappeApp {
   /** Name of the Frappe App instance */
   readonly name: string;
 
-  constructor(url: string, name?: string) {
+  /** Whether to use the token from the window object */
+  readonly useToken: boolean;
+
+  /** Token to be used for authentication */
+  readonly token?: () => string;
+
+  /** Type of token to be used for authentication */
+  readonly tokenType?: 'Bearer' | 'Token'
+
+  constructor(url: string, name?: string, useToken?: boolean, token?: () => string, tokenType?: 'Bearer' | 'Token') {
     this.url = url;
     this.name = name ?? 'FrappeApp';
+    this.useToken = useToken ?? false;
+    this.token = token;
+    this.tokenType = tokenType;
   }
 
   /** Returns a FrappeAuth object for the app */
   auth() {
-    return new FrappeAuth(this.url);
+    return new FrappeAuth(this.url, this.useToken, this.token, this.tokenType);
   }
   /** Returns a FrappeDB object for the app */
   db() {
-    return new FrappeDB(this.url);
+    return new FrappeDB(this.url, this.useToken, this.token, this.tokenType);
   }
 
   file() {
-    return new FrappeFileUpload(this.url);
+    return new FrappeFileUpload(this.url, this.useToken, this.token, this.tokenType);
   }
 
   call() {
-    return new FrappeCall(this.url);
+    return new FrappeCall(this.url, this.useToken, this.token, this.tokenType);
   }
 }
